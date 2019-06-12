a1048fd8bb262081d67f925b5f0f16972909399a
Abhishek Payasi <Abhishek Payasi@DESKTOP-AA50EH2>
2019-06-12 16:40:52 +0530

    ss

--- src/com/app/test/Test.java
+++ src/com/app/test/Test.java
@@ -3,6 +3,8 @@ package com.app.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello abhishek");
-		
+		int a=0;
+		System.out.println(a);
+		System.out.println(a);System.out.println(a);System.out.println(a);
 	}
 }
